88bc202b185b6271e5485c5a82bea5be06e5a89b
Mateusz Kwieciński <mateusz.kwiecinski@tooploox.com>
2019-06-14 14:58:53 +0200

    Fix with workaround

--- library/src/main/java/pls/help/butterknife/LibraryActivity.kt
+++ library/src/main/java/pls/help/butterknife/LibraryActivity.kt
@@ -1,13 +1,18 @@
 package pls.help.butterknife
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import pls.help.butterknife.library.R
 import pls.help.butterknife.library.R2
 
 class LibraryActivity : AppCompatActivity() {
+
+    @BindView(R2.id.willWork)
+    lateinit var thisNeedsToBeHere: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,11 @@ class LibraryActivity : AppCompatActivity() {
 
     @OnClick(R2.id.btnInLibrary)
     fun start(){
+        onBackPressed()
+    }
+
+    @OnClick(R2.id.willWork)
+    fun start2(){
         onBackPressed()
     }
 }
